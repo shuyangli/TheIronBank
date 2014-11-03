9272285ce4352a257b2877dbbf3c7670382bf200
trackstar192 <zwaterso@nd.edu>
2014-11-02 21:21:26 -0500

    initial query success

--- partials/connect.php
+++ partials/connect.php
@@ -15,13 +15,20 @@ if($link->connect_errno) {
 	echo "failed connection".$link->connect_errno.":".$link->connect_error;
 }
 
-echo $link;
+$test_sql = "SELECT * FROM FM_Person;";
 
-// $test_sql = "SELECT * FROM FM_Person;";
+$result = $link->query($test_sql) or die($link->error.__LINE__);
 
-// $result = $link->query($test_sql) or die($link->error.__LINE__);
+echo '<table border="1">';
+while($tuple = mysqli_fetch_array($result, MYSQL_ASSOC)) {
+        echo '<tr>';
+        foreach($tuple as $colval) {
+                echo '<td>'.$colval.'</td>';
+        }
+        echo '</tr>';
+}
+echo '</table>';
 
-// echo $result;
 
 //prepare statement
 // $stmt = $link->prepare("insert into FM_Person (age) values (?);");
