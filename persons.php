b3d027ef5abc4f9b19940ffcb73a7e5e9a931fd7
trackstar192 <zwaterso@nd.edu>
2014-11-02 22:00:39 -0500

    persons table view

--- persons.php
+++ persons.php
@@ -10,7 +10,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Films</title>
+    <title>Persons</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -32,16 +32,16 @@
 
 <body>
 
-	<h1>Films</h1>
+	<h1>Persons</h1>
 
 	<?php
 
-	$test_sql = "SELECT * FROM FM_Film;";
+	$test_sql = "SELECT * FROM FM_Person;";
 
 	$result = $link->query($test_sql) or die($link->error.__LINE__);
 
 	echo '<table class="table table-striped">';
-	echo '<thead><tr><th>IMDB_ID</th><th>Poster URL</th><th>Description</th><th>Runtime (in min)</th><th>MPAA Rating</th><th>Gross Revenue (in $)</th><th>Release Year</th><th>Award Score</th><th>Title</th><th>Distributor</th></tr></thead>';
+	echo '<thead><tr><th>Person_ID</th><th>Name</th><th>Award Score</th></tr></thead>';
 
 	while($tuple = mysqli_fetch_array($result, MYSQL_ASSOC)) {
 	        echo '<tr>';
