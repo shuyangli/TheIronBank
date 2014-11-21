9ead84b1a4d5ef6a4c6f0a62870bf92b2b3331e3
trackstar192 <zwaterso@nd.edu>
2014-11-20 22:51:01 -0500

    set a string to an int, oops

--- update_person.php
+++ update_person.php
@@ -3,7 +3,7 @@ include("partials/connect.php");
 
 $query = $link->prepare("UPDATE FM_Person SET Person_Name = ?, Num_Awards = ? WHERE Person_ID = ? ");
 
-$query->bind_param("isi", $_POST['Person_Name'], $_POST['Num_Awards'], $_POST['Person_ID']);
+$query->bind_param("sii", $_POST['Person_Name'], $_POST['Num_Awards'], $_POST['Person_ID']);
 
 // $query = $link->prepare("UPDATE FM_Film SET MPAA_Rating = ? WHERE IMDB_ID = ? ");
 
