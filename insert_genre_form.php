ff8649f8bcbc27bf108013d5a089c9284170d906
Kyle Kozak <kkozak@nd.edu>
2014-11-21 12:51:52 -0500

    more insert_genre and insert form shennanigans

---
+++ insert_genre_form.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+<form action="insert_genre.php" method="get">
+Genre Name: <input type="textbox" name="g_name"/><br/>
+IMDB ID: <input type="textbox" name="imdb_id"/><br/>
+<input type="submit"/>
+</form>
+</body>
+</html>
