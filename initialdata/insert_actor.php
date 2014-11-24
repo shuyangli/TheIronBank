a35f1134107457568bc3cb9818ad16684043a131
Shuyang Li <shuyang.li.95@gmail.com>
2014-11-24 12:34:38 -0500

    Debug a case where each actor is only inserted once in FM_Acted_In

--- initialdata/insert_actor.php
+++ initialdata/insert_actor.php
@@ -3,12 +3,9 @@
 include("../partials/connect.php");
 
 // Read genre data
-$actor_data_file = file("actor_data", FILE_SKIP_EMPTY_LINES);
-
+$actor_data_file = file("actor_data_sample", FILE_SKIP_EMPTY_LINES);
 
 $person_fetch_stmt = $link->prepare("SELECT Person_ID FROM FM_Person WHERE Person_Name = ?");
-$insert_actor_stmt = $link->prepare("INSERT INTO FM_Person (Person_Name, Num_Awards) VALUES (?, 0)");
-$insert_relationship_stmt = $link->prepare("INSERT INTO FM_Acted_In (Person_ID, IMDB_ID) values (?, ?)");
 
 foreach ($actor_data_file as $idx => $val_str) {
 	$pair = explode('|', $val_str);
@@ -16,6 +13,7 @@ foreach ($actor_data_file as $idx => $val_str) {
 	$imdb_id = trim($pair[0]);
 
 	// Check if the actor exists
+	echo "Checking $actor_name\n";
 	$person_fetch_stmt->bind_param("s", $actor_name);
 	$person_fetch_stmt->execute();
 
@@ -25,15 +23,20 @@ foreach ($actor_data_file as $idx => $val_str) {
 
 	// The actor doesn't exist, we need to insert him/her first
 	if ($person_id == 0) {
-		$insert_actor_stmt->bind_param("s", $actor_name);
-		$insert_actor_stmt->execute();
+		echo "$actor_name doesn't exist\n";
+
+		if ($insert_actor_stmt = $link->prepare("INSERT INTO FM_Person (Person_Name, Num_Awards) VALUES (?, 0)")) {
+			echo "Inserting\n"
+			$insert_actor_stmt->bind_param("s", $actor_name);
+			$insert_actor_stmt->execute();
+		}
 
 		// Then after insert, get the insert id to be used
 		$person_id = $link->insert_id;
 	}
 
 	// Then insert the relationship into FM_Acted_In
-	if ($insert_relationship_stmt) {
+	if ($insert_relationship_stmt = $link->prepare("INSERT INTO FM_Acted_In (Person_ID, IMDB_ID) values (?, ?)")) {
 		$insert_relationship_stmt->bind_param("ss", $person_id, $imdb_id);
 		$insert_relationship_stmt->execute();
 	} else {
