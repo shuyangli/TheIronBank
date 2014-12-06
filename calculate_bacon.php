274a2d57876fda73fc34cad12714c6fe4a2782c3
trackstar192 <zwaterso@nd.edu>
2014-12-06 17:09:08 -0500

    debug bacon

--- calculate_bacon.php
+++ calculate_bacon.php
@@ -3,7 +3,9 @@ include('partials/connect.php');
 
 //get films acted by person
 
-$query = $link->prepare("SELECT IMDB_ID FROM FM_Acted_In WHERE Person_ID = ( SELECT Person_ID FROM FM_Person WHERE Person_Name = ?");
+$query = $link->prepare("SELECT IMDB_ID FROM FM_Acted_In WHERE Person_ID = ( SELECT Person_ID FROM FM_Person WHERE Person_Name = ? ");
+
+var_dump($query);
 
 $query->bind_param("s", $_POST['firstPersonName']);
 
