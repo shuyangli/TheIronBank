7bf2f41c4470c8e3fb539cc4bfd9894a065bcd34
Kyle Kozak <kkozak@nd.edu>
2014-12-06 15:29:18 -0500

    added sidebar to add a person

--- insert_person_form.php
+++ insert_person_form.php
@@ -1,9 +1,63 @@
-<html>
+<?php include('partials/connect.php') ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Add A Person</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/simple-sidebar.css" rel="stylesheet">
+    
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+    
+    <!-- Sidebar JS-->
+    <script src="js/sidebar.js"></script>
+
+
+</head>
+
 <body>
-<form action="insert_person.php" method="get">
-Name: <input type="textbox" name="name"/><br/>
-Number of Awards: <input type="textbox" name="numawards"/><br/>
-<input type="submit"/>
-</form>
+
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <?php include('partials/sidebar.php') ?>
+        <!-- /#sidebar-wrapper -->
+
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1>Add A Person</h1>
+                        <form action="calculate_bacon.php" method="get">
+                        Name: <input type="textbox" name="name"/><br/>
+						Number of Awards: <input type="textbox" name="numawards"/><br/>
+                        <input type="submit"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /#page-content-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
 </body>
+
 </html>
