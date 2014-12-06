b0a0ae7cfb6193360026bfa13c868c2b0c6dd822
Shuyang Li <shuyang.li.95@gmail.com>
2014-12-06 16:42:35 -0500

    Requesting director and writer data

---
+++ initialdata/request_director_writer.rb
@@ -0,0 +1,74 @@
+require 'net/http'
+require 'uri'
+require 'thread'
+require 'json'
+
+file_content = []
+
+NUM_OF_THREADS = 40
+mutex = Mutex.new
+
+# Read file
+File.open("imdb_ids").each do |line|
+	file_content << line
+end
+
+# Write file
+director_file = File.open("director_data", "w")
+writer_file = File.open("writer_data", "w")
+
+# Casually parallelize networking
+
+NUM_OF_THREADS.times.map {
+
+	Thread.new(file_content) { |file_content|
+
+		while imdb_id = mutex.synchronize { file_content.pop }
+
+			# Form the URI
+			uri = URI.parse(URI.escape "http://www.imdbapi.com/?t=#{imdb_id}")
+
+			# Request
+			response = Net::HTTP.get_response(uri)
+			response_json_string = response.body
+
+			begin
+				# This gives us a JSON::ParserError with improperly escaped JSON response
+				response_json = JSON.parse(response.body)
+			rescue JSON::ParserError
+				next
+			end
+
+			# If the film doesn't exist, skip;
+			# otherwise add it
+			if (response_json["Response"] == "True") then
+
+				new_film = {}
+				new_film[:director] = if response_json["Director"] then response_json["Director"] end
+				new_film[:writer] = if response_json["Writer"] then response_json["Writer"].split(',').map( |name| name.strip ) end
+				new_film[:writer].each do |name|
+					name.gsub!(/\(.*\)/, "")
+				end
+
+				# Print out stuff
+				mutex.synchronize {
+					new_film[:director].each do |director|
+						puts "#{imdb_id}|#{director}"
+						# director_file.write("#{imdb_id}|#{director}")
+					end
+
+					new_film[:writer].each do |writer|
+						puts "#{imdb_id}|#{writer}"
+						# writer_file.write("#{imdb_id}|#{writer}")
+					end
+				}
+
+			end
+		end
+
+	}
+
+}.each(&:join)
+
+director_file.close
+writer_file.close
