1a04028cd65d0e90ef8328ca8d0e56d5b95726f8
lmille14 <luke@lukes-mbp.dhcp.nd.edu>
2014-12-08 18:45:10 -0500

    prediction uses distributor and writer

--- prediction_calculator.php
+++ prediction_calculator.php
@@ -4,7 +4,7 @@ include('partials/connect.php');
 
 //parse input data
 $db_directors = $_GET['director'];
-$db_writer = $_GET['writer'];
+$db_writersArray = explode(',',$_GET['writer']);
 $db_distributor = $_GET['distributor'];
 $db_rating = $_GET['rating'];
 $db_genre = $_GET['genre'];
@@ -17,9 +17,13 @@ for ($i = 0; $i < count($db_actorsArray); ++$i){
     $db_actorsArray[$i] = trim($db_actorsArray[$i]);
     print $db_actorsArray[$i] . "<br>";
 }
+print "<br>Writers: <br>";
+for ($i = 0; $i < count($db_writersArray); ++$i){
+    $db_writersArray[$i] = trim($db_writersArray[$i]);
+    print $db_writersArray[$i] . "<br>";
+}
 
 print "<br>Director is " . $db_directors . "<br>";
-print "Writer is " . $db_writer . "<br>";
 print "Distributor is " . $db_distributor . "<br>";
 print "Rating is " . $db_rating . "<br>";
 print "Genre is " . $db_genre . "<br>";
@@ -28,49 +32,86 @@ print "Release Year is " . $db_releaseYear . "<br>";
 //Perform Linear Regression to make predictions
 $estimates = array(); //Will hold estimates for gross based upon each input
 
-//Distributor
+//--Distributor--
 $query = "select Gross from FM_Film where Distributor=? and Gross!='null' and Release_Year>=? order by Release_Year desc limit 25;";
 
 if ($stmt = $link->prepare($query)){
     $stmt->bind_param("si", $db_distributor, $db_relevantDecade);
     $stmt->execute();
     $stmt->store_result();
-    $rowCount = $stmt->num_rows;
     $result = $stmt->bind_result($gross);
 
     $sum = 0;
     $count = 0;
 
-    //for ($i = 0; $i < count($grosses); ++$i){
     while($stmt->fetch()){ 
-        print $gross . "<br>";
         if ($gross>0){
             $count = $count +1;
             $sum = $sum + $gross;
         }
     }
 
-    //while ($tuple = $result->fetch_assoc()){
-    //    print "<br>";
-    //    foreach ($tuple as $key => $value) {
-    //        if ($key=="Gross" && $value>0){
-    //            $count = $count +1;
-    //            $sum = $sum + $value;
-    //        }
-    //        print $key . "\t" . $value . "<br>";
-    //    }
-    //}
     if ($count>0){
         array_push($estimates, $sum/$count);
     }
     $stmt->free_result();
     $stmt->close();
-    $link->close();
 }
 
+//--Writers--
+$sum = 0;
+$count = 0;
 
+for ($i = 0; $i < count($db_writersArray); ++$i){
+    $writer = $db_writersArray[$i]
+    $query = "select Person_ID from FM_Persons where Person_Name=?;";
+    if ($stmt = $link->prepare($query)){
+        $stmt->bind_param("s", $writer);
+        $stmt->execute();
+        $stmt->store_result();
+        $stmt->bind_result($writer_id);
+        while($stmt->fetch()){ 
+            if ($writer_id>0){
+                //Get movie Id from writer table
+                $query2 = "select IMDB_ID from FM_Wrote where Person_ID=?;";
+                if ($stmt2 = $link->prepare($query2)){
+                    $stmt2->bind_param("s", $writer_id);
+                    $stmt2->execute();
+                    $stmt2->store_result();
+                    $stmt2->bind_result($movie_id);
+                    while($stmt2->fetch()){ 
+                        if ($movie_id>0){
+                            //Get gross from FM_film using movie_id
+                            $query3 = "select Gross from FM_Film where IMBD_ID=? and Gross!='null' and Release_Year>=? order by Release_Year desc limit 25;";
+                            if ($stmt3 = $link->prepare($query3)){
+                                $stmt3->bind_param("ii", $movie_id, $db_relevantDecade);
+                                $stmt3->execute();
+                                $stmt3->store_result();
+                                $stmt->bind_result($gross);
 
-//Writers
+                                while($stmt3->fetch()){ 
+                                    if ($gross>0){
+                                        $count = $count +1;
+                                        $sum = $sum + $gross;
+                                    }
+                                }
+                                $stmt3->free_result();
+                                $stmt3->close();
+                            }
+                        }
+                    }
+                    $stmt2->free_result();
+                    $stmt2->close();
+                }
+            }
+        }
+        $stmt->free_result();
+        $stmt->close();
+    }
+}
+if ($count>0){
+    array_push($estimates, $sum/$count);
+}
 
 //Rating
 
@@ -78,6 +119,7 @@ if ($stmt = $link->prepare($query)){
 
 //Actors
 
+$link->close();
 print_r($estimates);
 
 ?>
