e1cfe858c8505f0811c6f7d3d5ed29c497c36549
Shuyang Li <shuyang.li.95@gmail.com>
2014-12-09 18:22:30 -0500

    Initial implementation of actor-based searching functionality

--- search_form.php
+++ search_form.php
@@ -18,16 +18,42 @@
 
     <!-- Custom CSS -->
     <link href="css/simple-sidebar.css" rel="stylesheet">
-    
+
     <!-- jQuery -->
     <script src="js/jquery.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
-    
+
     <!-- Sidebar JS-->
     <script src="js/sidebar.js"></script>
 
+    <!-- AJAX request to search for information -->
+    <script>
+        $("#search_submit").on('click', function (e) {
+            // Prevent default
+            e.preventDefault();
+
+            // AJAX call
+            $.ajax({
+                url: 'search_actor.php',
+                type: 'GET',
+                dataType: 'json',
+                data: { name: $("#name_input").text() },
+                success: function(result) {
+
+                    $("#result-container").empty();
+
+                    // Populate result container
+                    resArray = JSON.parse(result);
+
+                    for (var i = 0; i < resArray.length; i += 1) {
+                        $("#result-container").append("ID: " + resArray[i][0] + ", Name: " + resArray[i][1] + "<br />");
+                    }
+                }
+            });
+        });
+    </script>
 
 </head>
 
@@ -45,10 +71,13 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <h1>Search</h1>
-                        <form action="search.php" method="get">
-                        Name: <input type="textbox" name="name"/><br/>
-                        <input type="submit"/>
+                        <form method="get">
+                        Name: <input type="textbox" name="name" id="name_input" /><br/>
+                        <input type="submit" id="search_submit"/>
                     </div>
+                </div>
+                <div class="row">
+                    <div id="result-container" class="col-lg-12"></div>
                 </div>
             </div>
         </div>
