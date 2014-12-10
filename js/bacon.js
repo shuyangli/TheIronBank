353794331e73dbf5c8f2e941207fe2d4c37b5b2a
trackstar192 <zwaterso@nd.edu>
2014-12-09 20:57:27 -0500

    convert to AJAX oh god

--- js/bacon.js
+++ js/bacon.js
@@ -1,20 +1,20 @@
- 
 $(document).ready(function() {
 
     $("#search_form").on('submit', function (e) {
 
-    // Prevent form from actually submitting
-    e.preventDefault();
+        // Prevent form from actually submitting
+        e.preventDefault();
 
-    // AJAX call to get actors
-    $.ajax({
-        url: 'calculate_bacon.php',
-        type: 'GET',
-        data: $("#baconForm").serialize(),
-        success: function(result) {
-            // Populate result container
-            resArray = JSON.parse(result);
-            console.log(resArray);
-        }
+        // AJAX call to get actors
+        $.ajax({
+            url: 'calculate_bacon.php',
+            type: 'GET',
+            data: $("#baconForm").serialize(),
+            success: function(result) {
+                // Populate result container
+                resArray = JSON.parse(result);
+                console.log(resArray);
+            }
+        });
     });
 }
