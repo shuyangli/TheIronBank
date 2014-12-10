19b4932f05189f1264fab7aee25c89ba98f3415a
lmille14 <luke@lukes-mbp.dhcp.nd.edu>
2014-12-09 22:33:38 -0500

    Removed whitespace

--- predictor.php
+++ predictor.php
@@ -18,13 +18,13 @@
 
     <!-- Custom CSS -->
     <link href="css/simple-sidebar.css" rel="stylesheet">
-    
+
     <!-- jQuery -->
     <script src="js/jquery.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
-    
+
     <!-- Sidebar JS-->
     <script src="js/sidebar.js"></script>
 
