6b333ece53a71ef42fbde3fd186e03f3a881a21b
lmille14 <luke@lukes-mbp.dhcp.nd.edu>
2014-12-09 22:10:19 -0500

    new line?

--- predictor.php
+++ predictor.php
@@ -51,7 +51,7 @@
                     $("#return-row").show();
                     // Populate result container
                     resArray = JSON.parse(result);
-                    $("#title-container").text("Domestic Gross Estimate is $" + resArray[0]+"<br/>Look Below for Recent Movies had Similar Domestic Gross:");
+                    $("#title-container").text("Domestic Gross Estimate is $" + resArray[0]+"\n\nLook Below for Recent Movies had Similar Domestic Gross:");
                     for (var i = 1; i < resArray.length; i += 1){
                         $("#res-container").append("<tr><td>" + resArray[i][0] + "</td><td>$" + resArray[i][1] + "</td></tr>");
                     }
