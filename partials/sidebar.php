98fecd40d4d461ac88a03c15b53ac70e6d1ec04f
Shuyang Li <shuyang.li.95@gmail.com>
2014-12-09 22:46:25 -0500

    Attempt to fix a weird display issue where predictor.php is shifted down with some whitespace

--- partials/sidebar.php
+++ partials/sidebar.php
@@ -1,45 +1,40 @@
-<!DOCTYPE html>
-<html lang="en">
-
-        <!-- Sidebar -->
-        <div id="sidebar-wrapper">
-            <!-- Custom CSS -->
-            <ul class="sidebar-nav">
-                <li class="sidebar-brand">
-                    <a href="index.php">FMDB</a>
-                </li>
-                <li>
-                    <a href="search_form.php">Search</a>
-                </li>
-                <li>
-                    <a href="advanced_search_form.php">Advanced Search</a>
-                </li>
-                <li>
-                    <a href="bacon.php">Bacon Calculator</a>
-                </li>
-                <li>
-                    <a href="predictor.php">Predictor</a>
-                </li>
-                <li>
-                    <a href="films.php">View Films</a>
-                </li>
-                <li>
-                    <a href="insert_film_form.php">Add A Film</a>
-                </li>
-                <li>
-                    <a href="persons.php">View Persons</a>
-                </li>
-                <li>
-                    <a href="insert_person_form.php">Add A Person</a>
-                </li>
-                <li>
-                    <a href="genres.php">View Genres</a>
-                </li>
-                <li>
-                    <a href="insert_genre_form.php">Add A Genre</a>
-                </li>
-            </ul>
-        </div>
-        <!-- /#sidebar-wrapper -->
-
-</html>
+<!-- Sidebar -->
+<div id="sidebar-wrapper">
+<!-- Custom CSS -->
+<ul class="sidebar-nav">
+    <li class="sidebar-brand">
+        <a href="index.php">FMDB</a>
+    </li>
+    <li>
+        <a href="search_form.php">Search</a>
+    </li>
+    <li>
+        <a href="advanced_search_form.php">Advanced Search</a>
+    </li>
+    <li>
+        <a href="bacon.php">Bacon Calculator</a>
+    </li>
+    <li>
+        <a href="predictor.php">Predictor</a>
+    </li>
+    <li>
+        <a href="films.php">View Films</a>
+    </li>
+    <li>
+        <a href="insert_film_form.php">Add A Film</a>
+    </li>
+    <li>
+        <a href="persons.php">View Persons</a>
+    </li>
+    <li>
+        <a href="insert_person_form.php">Add A Person</a>
+    </li>
+    <li>
+        <a href="genres.php">View Genres</a>
+    </li>
+    <li>
+        <a href="insert_genre_form.php">Add A Genre</a>
+    </li>
+</ul>
+</div>
+<!-- /#sidebar-wrapper -->
